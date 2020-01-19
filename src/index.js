35fcbb18ebca0aeb73554630a72531192673ac88
Diogo Kollross <diogoko@gmail.com>
2020-01-18 21:17:20 -0300

    Ajusta o layout

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function atualizarSinais() {
     const texto = document.getElementById('textareaTexto').value;
     const canvas = document.getElementById('canvasSinais');
     const context = canvas.getContext('2d');
-    const fontSize = 30;
+    const fontSize = 48;
     const alturaLinha = fontSize * 1.0;
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.font = `${fontSize}px Libras-2016`;
@@ -30,6 +30,7 @@ function atualizarSinais() {
         context.fillText(linhas[i], canvas.width / 2, y, canvas.width);
         y += alturaLinha;
     }
+    console.log('escreveu');
 }
 
 
@@ -50,12 +51,23 @@ class Pagina {
         return document.getElementById('menuSinais');
     }
 
+    get divTexto() {
+        return document.getElementById('divTexto');
+    }
+
+    get divSinais() {
+        return document.getElementById('divSinais');
+    }
+
     constructor() {
         this.mostrarCanvasSinais = this.mostrarCanvasSinais.bind(this);
         this.mostrarTextareaTexto = this.mostrarTextareaTexto.bind(this);
+        this.redimensionarCanvas = this.redimensionarCanvas.bind(this);
     }
 
     configurarEventos() {
+        window.addEventListener('resize', this.redimensionarCanvas);
+
         let hammerTextareaTexto = new Hammer(this.textareaTexto);
         hammerTextareaTexto.on('panleft', this.mostrarCanvasSinais);
 
@@ -67,20 +79,36 @@ class Pagina {
         this.menuSinais.addEventListener('click', this.mostrarCanvasSinais);
     }
 
-    mostrarCanvasSinais() {
-        this.menuTexto.classList.remove('selecionado');
-        this.menuSinais.classList.add('selecionado');
-        console.log('sinais');
-    }
-    
     mostrarTextareaTexto() {
         this.menuTexto.classList.add('selecionado');
         this.menuSinais.classList.remove('selecionado');
-        console.log('texto');
+
+        this.divTexto.style.display = '';
+        this.divSinais.style.display = 'none';
+        //this.divTexto.style.visibility = 'visible';
+        //this.divSinais.style.visibility = 'hidden';
     }
     
+    mostrarCanvasSinais() {
+        this.menuTexto.classList.remove('selecionado');
+        this.menuSinais.classList.add('selecionado');
+
+        this.divTexto.style.display = 'none';
+        this.divSinais.style.display = '';
+        //this.divTexto.style.visibility = 'hidden';
+        //this.divSinais.style.visibility = 'visible';
+
+        setTimeout(atualizarSinais, 1000);
+    }
+    
+    redimensionarCanvas() {
+        const main = document.querySelector('main');
+        this.canvasSinais.width = main.offsetWidth;
+        this.canvasSinais.height = main.offsetHeight;
+    }
 }
 
 let pagina = new Pagina();
 pagina.configurarEventos();
 pagina.mostrarTextareaTexto();
+pagina.redimensionarCanvas();
