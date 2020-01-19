4bb1bf7ebfc121e51b2bfb31e44c0ce977d8670c
Diogo Kollross <diogoko@gmail.com>
2020-01-19 15:49:24 -0300

    Configura build

---
+++ webpack.dev.js
@@ -0,0 +1,10 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './docs',
+    },
+};
