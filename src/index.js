eca643f5c1314ed80e4bb8fb3fb817287962f845
Diogo Kollross <diogoko@gmail.com>
2020-01-19 06:53:09 -0300

    Corrige e incorpora desenho à classe

--- src/index.js
+++ src/index.js
@@ -1,39 +1,5 @@
 import Hammer from 'hammerjs';
 
-function formatarLinha(linha) {
-    return linha.normalize('NFD')
-        .replace(/[\u0300-\u036f]/g, '')
-        .toUpperCase()
-        .replace(/[^A-Z0-9]+/g, ' ')
-        .trim()
-}
-
-function formatarTexto(texto) {
-    return texto.split('\n').map(formatarLinha);
-}
-
-function atualizarSinais() {
-    const texto = document.getElementById('textareaTexto').value;
-    const canvas = document.getElementById('canvasSinais');
-    const context = canvas.getContext('2d');
-    const fontSize = 48;
-    const alturaLinha = fontSize * 1.0;
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.font = `${fontSize}px Libras-2016`;
-    context.fillStyle = 'blue';
-    context.textAlign = 'center';
-
-    const linhas = formatarTexto(texto);
-    const alturaTexto = linhas.length * alturaLinha;
-    let y = (canvas.height - alturaTexto) / 2 + alturaLinha;
-    for (let i = 0; i < linhas.length; i++) {
-        context.fillText(linhas[i], canvas.width / 2, y, canvas.width);
-        y += alturaLinha;
-    }
-    console.log('escreveu');
-}
-
-
 class Pagina {
     get textareaTexto() {
         return document.getElementById('textareaTexto');
@@ -63,6 +29,10 @@ class Pagina {
         this.mostrarCanvasSinais = this.mostrarCanvasSinais.bind(this);
         this.mostrarTextareaTexto = this.mostrarTextareaTexto.bind(this);
         this.redimensionarCanvas = this.redimensionarCanvas.bind(this);
+        this.formatarTexto = this.formatarTexto.bind(this);
+        this.atualizarSinais = this.atualizarSinais.bind(this);
+
+        this.jaDesenhou = false;
     }
 
     configurarEventos() {
@@ -83,22 +53,30 @@ class Pagina {
         this.menuTexto.classList.add('selecionado');
         this.menuSinais.classList.remove('selecionado');
 
-        this.divTexto.style.display = '';
-        this.divSinais.style.display = 'none';
+        //this.divTexto.style.display = '';
+        //this.divSinais.style.display = 'none';
         //this.divTexto.style.visibility = 'visible';
         //this.divSinais.style.visibility = 'hidden';
+        this.divTexto.style.zIndex = 2;
+        this.divSinais.style.zIndex = 1;
     }
     
     mostrarCanvasSinais() {
         this.menuTexto.classList.remove('selecionado');
         this.menuSinais.classList.add('selecionado');
 
-        this.divTexto.style.display = 'none';
-        this.divSinais.style.display = '';
+        //this.divTexto.style.display = 'none';
+        //this.divSinais.style.display = '';
         //this.divTexto.style.visibility = 'hidden';
         //this.divSinais.style.visibility = 'visible';
+        this.divTexto.style.zIndex = 1;
+        this.divSinais.style.zIndex = 2;
 
-        setTimeout(atualizarSinais, 1000);
+        this.atualizarSinais();
+        if (!this.jaDesenhou) {
+            this.jaDesenhou = true;
+            setTimeout(this.atualizarSinais, 100);
+        }
     }
     
     redimensionarCanvas() {
@@ -106,6 +84,39 @@ class Pagina {
         this.canvasSinais.width = main.offsetWidth;
         this.canvasSinais.height = main.offsetHeight;
     }
+
+    formatarLinha(linha) {
+        return linha.normalize('NFD')
+            .replace(/[\u0300-\u036f]/g, '')
+            .toUpperCase()
+            .replace(/[^A-Z0-9]+/g, ' ')
+            .trim()
+    }
+    
+    formatarTexto(texto) {
+        return texto.split('\n').map(this.formatarLinha);
+    }
+    
+    atualizarSinais() {
+        const texto = this.textareaTexto.value;
+        const canvas = this.canvasSinais;
+        const context = canvas.getContext('2d');
+        const fontSize = 48;
+        const alturaLinha = fontSize * 1.0;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.font = `${fontSize}px Libras-2016`;
+        context.fillStyle = 'blue';
+        context.textAlign = 'center';
+    
+        const linhas = this.formatarTexto(texto);
+        const alturaTexto = linhas.length * alturaLinha;
+        let y = (canvas.height - alturaTexto) / 2 + alturaLinha;
+        for (let i = 0; i < linhas.length; i++) {
+            context.fillText(linhas[i], canvas.width / 2, y, canvas.width);
+            y += alturaLinha;
+        }
+        console.log('escreveu');
+    }    
 }
 
 let pagina = new Pagina();
